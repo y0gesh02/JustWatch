6bbd0125b67292f522086680ad1967e294813960
yogeshsakle <yogeshsakle8@gmail.com>
2022-02-23 19:53:03 +0530

    Entertainment Hub

---
+++ src/components/Pagination/CustomPagination.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Pagination from "@material-ui/lab/Pagination";
+import {  ThemeProvider } from "@material-ui/core";
+import { createTheme } from '@material-ui/core/styles';
+
+const darkTheme = createTheme({
+  palette: {
+    type: "dark",
+  },
+});
+
+export default function CustomPagination({ setPage, numOfPages = 10 }) {
+  // Scroll to top when page changes
+  const handlePageChange = (page) => {
+    setPage(page);
+    window.scroll(0, 0);
+  };
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        marginTop: 50,
+      }}
+    >
+      <ThemeProvider theme={darkTheme}>
+        <Pagination
+          onChange={(e) => handlePageChange(e.target.textContent)}
+          count={numOfPages}
+          color="primary"
+          hideNextButton
+          hidePrevButton
+        />
+      </ThemeProvider>
+    </div>
+  );
+}
