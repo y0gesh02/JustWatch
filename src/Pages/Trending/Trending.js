75593070a5643e67099c7aee78e529f29074e41e
yogeshsakle <yogeshsakle8@gmail.com>
2022-04-17 01:40:04 +0530

    UI changes

--- src/Pages/Trending/Trending.js
+++ src/Pages/Trending/Trending.js
@@ -13,6 +13,7 @@ const Trending = () => {
       `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}&page=${page}`
     );
 
+    
     setContent(data.results);
   };
 
