7c642ab21dd658e54524f01f2910783c906720d2
yogeshsakle <yogeshsakle8@gmail.com>
2022-04-17 22:12:27 +0530

    UI changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import "./Header.css";
 const Header = () => {
   return (
     <span onClick={() => window.scroll(0, 0)} className="header">
-       Entertainment Hub 
+       JustWatch
     </span>
   );
 };
